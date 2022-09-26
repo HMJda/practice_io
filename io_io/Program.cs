e8875997ef039d86b974233981daa11352623353
deu <deu@DESKTOP-21DROLK>
2022-09-26 10:46:44 +0900

    초기 커밋.

---
+++ io_io/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Temp\vs.png", FileMode.Open))
+            {
+                int size = (int)fs.Length;
+                byte[] buff = new byte[size];
+                // 이미지를 10번에 걸쳐 나누어 읽음
+                for (int i = 0; i < 10; i++)
+                {
+                    int n = fs.Read(buff, 0, size / 10);
+                    //....
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
